9a7a476a86104585ee988013ea49b0c950386b27
Frantisek Varadzin <varadzin@gmail.com>
2022-01-16 20:10:40 +0100

    Initial Commit

---
+++ Example Delegate pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example Delegate pattern
+//
+//  Created by Frantisek Varadzin on 16.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
