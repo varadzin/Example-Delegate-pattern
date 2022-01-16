b584ae9c59d095fb8b92429168b0f30974a1dd34
Frantisek Varadzin <varadzin@gmail.com>
2022-01-16 21:25:39 +0100

    Learning  Delegate Patterns

---
+++ Example Delegate pattern/SelectView.swift
@@ -0,0 +1,72 @@
+//
+//  SelectView.swift
+//  Example Delegate pattern
+//
+//  Created by Frantisek Varadzin on 16.01.22.
+//
+
+import UIKit
+
+protocol CarSelectDelegate {
+    func didSelectCar(image: UIImage, name: String)
+}
+
+class SelectView: UIViewController {
+    var selectionDelegate: CarSelectDelegate!
+    var logoAudiButton = UIButton()
+    var logoBugattiButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configAudiButton()
+        configBugattiButton()
+    }
+    
+    func configAudiButton() {
+        view.addSubview(logoAudiButton)
+        logoAudiButton.setImage(UIImage(named: "logoAudi"), for: .normal)
+        logoAudiButton.layer.cornerRadius = 8
+        logoAudiButton.clipsToBounds = true
+        logoAudiButton.layer.borderWidth = 0.5
+        logoAudiButton.layer.borderColor = UIColor.systemGray4.cgColor
+        logoAudiButton.addTarget(self, action: #selector(didTapAudiButton), for: .touchUpInside)
+        
+        logoAudiButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoAudiButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            logoAudiButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoAudiButton.heightAnchor.constraint(equalToConstant: 100),
+            logoAudiButton.widthAnchor.constraint(equalToConstant: 250)
+        ])
+    }
+    
+    @objc func didTapAudiButton() {
+        selectionDelegate.didSelectCar(image: UIImage(named: "audi")!, name: "Audi")
+        dismiss(animated: true, completion: nil)
+    }
+   
+    
+    func configBugattiButton() {
+        view.addSubview(logoBugattiButton)
+        logoBugattiButton.setImage(UIImage(named: "logoBugatti"), for: .normal)
+        logoBugattiButton.layer.cornerRadius = 8
+        logoBugattiButton.clipsToBounds = true
+        logoBugattiButton.layer.borderWidth = 0.5
+        logoBugattiButton.layer.borderColor = UIColor.systemGray4.cgColor
+        logoBugattiButton.addTarget(self, action: #selector(didTapBugattiButton), for: .touchUpInside)
+        
+        logoBugattiButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoBugattiButton.topAnchor.constraint(equalTo: logoAudiButton.bottomAnchor, constant: 50),
+            logoBugattiButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoBugattiButton.heightAnchor.constraint(equalToConstant: 100),
+            logoBugattiButton.widthAnchor.constraint(equalToConstant: 250)
+        ])
+    }
+    
+    @objc func didTapBugattiButton() {
+        selectionDelegate.didSelectCar(image: UIImage(named: "bugatti")!, name: "Bugatti")
+        dismiss(animated: true, completion: nil)
+    }
+}
--- Example Delegate pattern/ViewController.swift
+++ Example Delegate pattern/ViewController.swift
@@ -8,12 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var titleLabel = UILabel()
+    var vehicleImage = UIImageView()
+    var selectButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configTitle()
+        configVehicleImage()
+        configButton()
     }
+    
+    func configTitle() {
+        view.addSubview(titleLabel)
+        titleLabel.text = "What will you drive today?"
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.systemFont(ofSize: 24)
+        titleLabel.textColor = .systemGreen
+ 
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 50),
+            titleLabel.widthAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+    
+    func configVehicleImage() {
+        view.addSubview(vehicleImage)
+        vehicleImage.layer.cornerRadius = 10
+        vehicleImage.clipsToBounds = true
+        vehicleImage.image = UIImage(named: "car")
+        
+        vehicleImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            vehicleImage.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 50),
+            vehicleImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            vehicleImage.heightAnchor.constraint(equalToConstant: 300),
+            vehicleImage.widthAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+    func configButton() {
+        view.addSubview(selectButton)
+        selectButton.layer.cornerRadius = 15
+        selectButton.backgroundColor = .systemGreen
+        selectButton.setTitle("Choose you car", for: .normal)
+        selectButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+        selectButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            selectButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            selectButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            selectButton.heightAnchor.constraint(equalToConstant: 50),
+            selectButton.widthAnchor.constraint(equalToConstant: 250)
+        ])
+    }
+    
+    @objc func didTapButton() {
+       let destinationViewControler = SelectView()
+        let navigationController = UINavigationController(rootViewController: destinationViewControler)
 
-
+        present(navigationController, animated: true)
+    }
+    
 }
 
+extension ViewController: CarSelectDelegate {
+    func didSelectCar(image: UIImage, name: String) {
+        titleLabel.text = "You chosed \(name) for today"
+        vehicleImage.image = image
+    }
+}
